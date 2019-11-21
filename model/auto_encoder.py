0136d581a6cb583488d6cb70167712af85ca58fa
Philip Ossenkopp <Philip.Ossenkopp@novatec-gmbh.de>
2019-11-21 17:38:11 +0100

    Repair AutoEncoder #11

--- model/auto_encoder.py
+++ model/auto_encoder.py
@@ -1,26 +1,19 @@
 import tensorflow as tf
-from tensorflow.keras import layers, Model
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Input, Dense
 
 
-def AutoEncoder(input_size, n_hidden, activation, loss='sparse_categorical_crossentropy', kernel_regularizer=None,
-                activity_regularizer=None, kernel_constraint=None, lr=1e-4, metrics=None):
-    if metrics is None:
-        metrics = ['accuracy']
-    input_layer = layers.Input(input_size)
-    hidden = layers.Dense(n_hidden, activation=activation, kernel_regularizer=kernel_regularizer,
+def AutoEncoder(input_size, n_hidden, activation, loss='binary_crossentropy', kernel_regularizer=None,
+                activity_regularizer=None, kernel_constraint=None, lr=1e-4, metrics=['accuracy']):
+    input_layer = Input(shape=input_size)
+    hidden = Dense(n_hidden, activation=activation, kernel_regularizer=kernel_regularizer,
                           activity_regularizer=activity_regularizer, kernel_constraint=kernel_constraint)(input_layer)
-
-    output_layer = layers.Dense(input_size, activation=activation, kernel_regularizer=kernel_regularizer,
-                                activity_regularizer=activity_regularizer, kernel_constraint=kernel_constraint)(hidden)
-
-    learner = Model(input_layer, output_layer)
-
+    output_layer = Dense(input_size[0], activation=activation, kernel_regularizer=kernel_regularizer,
+                          activity_regularizer=activity_regularizer, kernel_constraint=kernel_constraint)(hidden)
+    autoencoder = Model(input_layer, output_layer)
     # TODO: should the loss module be implemented here or is it applied on a per layer basis? Maybe change.
-    learner.compile(optimizer=tf.keras.optimizers.Adam(lr=lr),
-                    loss=loss,
-                    metrics=metrics)
-
-    return learner
+    autoencoder.compile(optimizer=tf.keras.optimizers.Adam(lr=lr), loss='binary_crossentropy', metrics=metrics)
+    return autoencoder
 
 
 def get_model(*args, **kwargs):
