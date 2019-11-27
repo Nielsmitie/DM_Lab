f7bd8c2497c3678da0bea81a53205f9d52be40b7
Nielsmitie <niels.nuthmann@googlemail.com>
2019-11-27 12:14:53 +0100

    Added output for number of classes

--- dataset/dummy_data.py
+++ dataset/dummy_data.py
@@ -3,9 +3,10 @@ import scipy
 
 
 def get_dataset(path):
+    num_classes = None
     mat = scipy.io.loadmat(path)
     X = mat['X']
     X = X.astype(float)
     y = mat['Y']
     y = y[:, 0]
-    return X, y
+    return X, y, num_classes
--- dataset/iris_loader.py
+++ dataset/iris_loader.py
@@ -3,4 +3,4 @@ from sklearn import datasets
 
 def get_dataset():
     iris = datasets.load_iris()
-    return iris.data, iris.target
+    return iris.data, iris.target, 3
