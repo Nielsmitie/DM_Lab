c3840f4aef28f83ff79e76975e07cd1060704795
Nielsmitie <niels.nuthmann@googlemail.com>
2020-02-17 15:22:36 +0100

    Fixed error with pandas loading. resulting in mixed r_square and acc values in one row

--- helper/pandas_helper.py
+++ helper/pandas_helper.py
@@ -12,7 +12,7 @@ def pd_read_multi_column(path_to_file, *args, **kwargs):
     r = df['r_square']
     r.columns = r.columns.astype('int')
 
-    df = pd.concat([acc, r, df['config']], axis=1, keys=['r_square', 'acc', 'config'])
+    df = pd.concat([acc, r, df['config']], axis=1, keys=['acc', 'r_square', 'config'])
 
     return df
 
