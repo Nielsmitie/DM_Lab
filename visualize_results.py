7f865f0919dc059b6f36bcfa79a4d6113371ae5a
Nielsmitie <niels.nuthmann@googlemail.com>
2020-02-18 10:38:08 +0100

    boxplot is now a function

--- visualize_results.py
+++ visualize_results.py
@@ -7,38 +7,41 @@ from helper import pandas_helper
 
 from helper.paper import datasets, acc_results, r2_results
 
-plt.style.use('fivethirtyeight')
-path_to_result_file = 'results/paper_agnos_s_reworked.csv'
-model = 'agnos_s'
+
+def box_plot_results(path_to_result_file='results/paper_agnos_s_reworked.csv', model='agnos_s'):
+    plt.style.use('fivethirtyeight')
+
+    paper = pd.concat([pd.DataFrame(acc_results, index=datasets).T, pd.DataFrame(r2_results, index=datasets).T],
+                      keys=['acc', 'r_square'], axis=1).stack()
+    paper = paper.loc[model]
+
+    df = pandas_helper.pd_read_multi_column(path_to_result_file)
+    df = df[[('acc', 100), ('r_square', 100), ('config', 'dataset')]]
+    df = df.T.reset_index(level=-1, drop=True).T
+    df.columns = ['acc', 'r_square', 'dataset']
+
+    df['dataset'] = df['dataset'].str.replace("mat_loader{'name': '", "").str.replace("'}", "")
+
+    test = [i for i in range(10)]
+    index = []
+    for i in range(8):
+        index.extend(test)
+
+    df['index'] = index
+
+    acc = df.pivot(values='acc', columns='dataset', index='index').astype(float)
+    r2 = df.pivot(values='r_square', columns='dataset', index='index').astype(float)
+    test = list(acc.columns)
+
+    plt.title('ACC')
+    acc.boxplot(fontsize=9)
+    plt.show()
+
+    plt.title('R2')
+    r2.boxplot(fontsize=9)
+    plt.show()
+
+    print('test')
 
 
-paper = pd.concat([pd.DataFrame(acc_results, index=datasets).T, pd.DataFrame(r2_results, index=datasets).T],
-                  keys=['acc', 'r_square'], axis=1).stack()
-paper = paper.loc['agnos_s']
-
-df = pandas_helper.pd_read_multi_column(path_to_result_file)
-df = df[[('acc', 100), ('r_square', 100), ('config', 'dataset')]]
-df = df.T.reset_index(level=-1, drop=True).T
-df.columns = ['acc', 'r_square', 'dataset']
-
-df['dataset'] = df['dataset'].str.replace("mat_loader{'name': '", "").str.replace("'}", "")
-
-
-test = [i for i in range(10)]
-index = []
-for i in range(8):
-    index.extend(test)
-
-df['index'] = index
-
-acc = df.pivot(values='acc', columns='dataset', index='index').astype(float)
-r2 = df.pivot(values='r_square', columns='dataset', index='index').astype(float)
-test = list(acc.columns)
-
-ax = acc.boxplot(fontsize=9)
-plt.show()
-
-r2.boxplot(fontsize=9)
-plt.show()
-
-print('test')
+box_plot_results()
