625809539f020055e2c6f0e359245250f60b2b38
Nielsmitie <niels.nuthmann@googlemail.com>
2020-02-23 10:57:29 +0100

    evaluation is now running in parallel for loop.

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,18 @@
 import numpy as np
 from skfeature.utility.unsupervised_evaluation import evaluation
 from sklearn.neighbors import KNeighborsRegressor
+import multiprocessing as mp
+
+
+# TODO: Also return NMI
+def score_acc(n_, X_, sorted_features_, num_clusters_, y_, repetitions_):
+    nmi_total = 0
+    acc_total = 0
+    for _ in range(repetitions_):
+        nmi, acc = evaluation(X_[:, sorted_features_[:n_]], num_clusters_, y_)
+        nmi_total += nmi
+        acc_total += acc
+    return n_, float(acc_total) / repetitions_
 
 
 def k_means_accuracy(X, y, num_clusters, sorted_features,
@@ -23,18 +35,23 @@ def k_means_accuracy(X, y, num_clusters, sorted_features,
     if not isinstance(top_n, list):
         top_n = [top_n]
 
-    # TODO: Also return NMI
-    results = {}
-    for n in top_n:
-        nmi_total = 0
-        acc_total = 0
-        for _ in range(repetitions):
-            nmi, acc = evaluation(X[:, sorted_features[:n]], num_clusters, y)
-            nmi_total += nmi
-            acc_total += acc
-        results[n] = float(acc_total) / repetitions
+    pool = mp.Pool(mp.cpu_count())
+
+    results = dict(pool.starmap(score_acc, [(n, X, sorted_features, num_clusters, y, repetitions) for n in top_n]))
 
     return results
+
+
+def score_r_square(n_, X_, sorted_features_):
+    r_scores = []
+    for feature in range(X_.shape[1]):
+        regressor = KNeighborsRegressor(
+            n_neighbors=5, weights='uniform', algorithm='auto', p=2)
+        target = X_[:, feature]
+        regressor.fit(X_[:, sorted_features_[:n_]], target)
+        r_scores.append(regressor.score(X_[:, sorted_features_[:n_]], target))
+    pred = np.mean(r_scores)
+    return n_, pred
 
 
 def r_squared(X, y, num_clusters, sorted_features, top_n=100):
@@ -56,16 +73,10 @@ def r_squared(X, y, num_clusters, sorted_features, top_n=100):
     if not isinstance(top_n, list):
         top_n = [top_n]
 
-    results = {}
-    for n in top_n:
-        r_scores = []
-        for feature in range(X.shape[1]):
-            regressor = KNeighborsRegressor(
-                n_neighbors=5, weights='uniform', algorithm='auto', p=2)
-            target = X[:, feature]
-            regressor.fit(X[:, sorted_features[:n]], target)
-            r_scores.append(regressor.score(X[:, sorted_features[:n]], target))
-        pred = np.mean(r_scores)
-        results[n] = pred
+    pool = mp.Pool(mp.cpu_count())
+
+    results = dict(pool.starmap(score_r_square, [(n, X, sorted_features) for n in top_n]))
+
+    print('r_square evaluation done. ({})'.format(results))
 
     return results
