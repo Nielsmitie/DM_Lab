6bbe73851ea5100a8731fe557c039619c3365c45
Nielsmitie <niels.nuthmann@googlemail.com>
2020-02-23 16:03:04 +0100

    Config file for agnos_w_curve Build script to draw the curves

--- visualize_eval_curve.py
+++ visualize_eval_curve.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from itertools import cycle
@@ -9,8 +8,28 @@ import os
 
 from helper import pandas_helper
 
-path_to_result_file = 'results/curves.csv'
-
+path_to_result_file = 'logs/curves.csv'
+metrics = ['acc', 'r_square']
+title = 'Results with different top n features for evaluation: '
+save_path = os.path.join('pics', 'curve')
 
 df = pandas_helper.pd_read_multi_column(path_to_result_file)
+df[('config', 'model')] = df[('config', 'model')].str.replace('{.*}', '')
+df.set_index(('config', 'model'), inplace=True)
+
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+
+for metric in metrics:
+    test = df[metric]
+
+    test.T.sort_index().plot(title=title)
+    plt.ylabel(metric)
+    plt.xlabel('Top n features')
+
+    plt.savefig(os.path.join(save_path, title.strip().replace(':', '') + '_' + metric + '.png'), bbox_inches='tight')
+
+    plt.show()
+
+print('test')
 
